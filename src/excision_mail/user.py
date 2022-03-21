de747b87f0064a55c85ccb027dba82734b2f76f0
Aisha Tammy <floss@bsd.ac>
2022-03-21 13:07:41 -0400

    combine vars into one dict
    Signed-off-by: Aisha Tammy <floss@bsd.ac>

--- src/excision_mail/user.py
+++ src/excision_mail/user.py
@@ -4,66 +4,65 @@ from .UserManager import UserManager, UStat
 from .utils import get_newpass
 
 
-def user_add(user: str, config: typing.Dict[str, str], *_, **__):
-    um = UserManager(**config)
+def user_add(user: str, **kwargs):
+    um = UserManager(**kwargs)
     newpass = get_newpass()
     um.user_add(user, newpass)
 
 
-def user_delete(user: str, config: typing.Dict[str, str], *_, **__):
-    um = UserManager(**config)
+def user_delete(user: str, **kwargs):
+    um = UserManager(**kwargs)
     um.user_delete(user)
 
 
-def user_disable(user: str, config: typing.Dict[str, str], *args, **kwargs):
-    um = UserManager(**config)
+def user_disable(user: str, **kwargs):
+    um = UserManager(**kwargs)
     um.user_disable(user)
 
 
-def user_enable(user: str, config: typing.Dict[str, str], *args, **kwargs):
-    um = UserManager(**config)
+def user_enable(user: str, **kwargs):
+    um = UserManager(**kwargs)
     um.user_enable(user)
 
 
-def alias_add(alias: str, config: typing.Dict[str, str], *_, **__):
-    um = UserManager(**config)
+def alias_add(alias: str, **kwargs):
+    um = UserManager(**kwargs)
     um.alias_add(alias)
 
 
-def alias_allow(user: str, alias: str, config: typing.Dict[str, str], *_,
-                **__):
-    um = UserManager(**config)
+def alias_allow(user: str, alias: str, **kwargs):
+    um = UserManager(**kwargs)
     um.alias_allow(user, alias)
 
 
-def alias_delete(alias: str, config: typing.Dict[str, str], *_, **__):
-    um = UserManager(**config)
+def alias_delete(alias: str, **kwargs):
+    um = UserManager(**kwargs)
     um.alias_delete(alias)
 
 
-def alias_deny(user: str, alias: str, config: typing.Dict[str, str], *_, **__):
-    um = UserManager(**config)
+def alias_deny(user: str, alias: str, **kwargs):
+    um = UserManager(**kwargs)
     um.alias_deny(user, alias)
 
 
-def alias_disable(alias: str, config: typing.Dict[str, str], *_, **__):
-    um = UserManager(**config)
+def alias_disable(alias: str, **kwargs):
+    um = UserManager(**kwargs)
     um.alias_disable(alias)
 
 
-def alias_enable(alias: str, config: typing.Dict[str, str], *_, **__):
-    um = UserManager(**config)
+def alias_enable(alias: str, **kwargs):
+    um = UserManager(**kwargs)
     um.alias_enable(alias)
 
 
-def change_passwd(user: str, config: typing.Dict[str, str], *_, **__):
-    um = UserManager(**config)
+def change_passwd(user: str, **kwargs):
+    um = UserManager(**kwargs)
     newpass = get_newpass()
     um.change_passwd(user, newpass)
 
 
-def account_info(account: str, config: typing.Dict[str, str], *_, **__):
-    um = UserManager(**config)
+def account_info(account: str, **kwargs):
+    um = UserManager(**kwargs)
     astatus = um.check_status(account)
 
     if astatus == UStat.USER:
@@ -75,6 +74,6 @@ def account_info(account: str, config: typing.Dict[str, str], *_, **__):
     elif astatus == UStat.EXTERNAL:
         print(f'{account} is an external user')
     elif astatus == UStat.ABSENT:
-        print(f'{acconut} is not present')
+        print(f'{account} is not present')
     else:
         print(f'{account} status unknown')
